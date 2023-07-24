a9ae8e5d7177f4a15f5fde7274e9c184f2bdc5f8
swapnil bhakare <67636219+swapnilbhakare@users.noreply.github.com>
2023-07-24 21:05:27 +0530

    Update App.js
    logout functionality done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import { Switch, Route } from 'react-router-dom';
-
+import { useContext } from "react";
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 
+import AuthContext from "./components/Store/Auth-Context";
+
 function App() {
+  return (
+    const AuthCtx = useContext(AuthContext);
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
+        {!AuthCtx.isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        <Route path="/profile">
+          {AuthCtx.isLoggedIn && <UserProfile />}
+          {!AuthCtx.isLoggedIn && <Redirect to="/auth" />}
         </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
