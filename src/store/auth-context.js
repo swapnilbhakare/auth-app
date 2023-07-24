53344cd6ed011dff2912ead73ae6a6c2b484e4d9
swapnil bhakare <67636219+swapnilbhakare@users.noreply.github.com>
2023-07-24 21:07:39 +0530

    Update auth-context.js
    auto logout done

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,15 +7,43 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
-
+  useEffect(() => {
+    let timer;
+    const resetTimer = () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        logoutHandler();
+      }, 60 * 1000);
+    };
+    const events = [
+      "mousedown",
+      "mousemove",
+      "keypress",
+      "scroll",
+      "touchstart",
+    ];
+    for (let event of events) {
+      window.addEventListener(event, resetTimer);
+    }
+    resetTimer();
+    return () => {
+      for (let event of events) {
+        window.removeEventListener(event, resetTimer);
+      }
+      clearTimeout(timer);
+    };
+  }, []);
   const ContextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
